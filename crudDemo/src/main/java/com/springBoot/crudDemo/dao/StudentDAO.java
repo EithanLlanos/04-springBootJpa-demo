d7275a44541855e9c437e0d01441651ecca60c1c
Rubick <eithanllanosb@gmail.com>
2024-12-06 11:51:41 -0500

    Add method to retrieve students

--- crudDemo/src/main/java/com/springBoot/crudDemo/dao/StudentDAO.java
+++ crudDemo/src/main/java/com/springBoot/crudDemo/dao/StudentDAO.java
@@ -2,10 +2,15 @@ package com.springBoot.crudDemo.dao;
 
 import com.springBoot.crudDemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
-    
+
     //Method to search on DB by Id
     Student findById(Integer id);
+
+    //Method to retrieve all
+    List<Student> findAll();
 }
--- crudDemo/src/main/java/com/springBoot/crudDemo/dao/StudentDAOImpl.java
+++ crudDemo/src/main/java/com/springBoot/crudDemo/dao/StudentDAOImpl.java
@@ -2,8 +2,11 @@ package com.springBoot.crudDemo.dao;
 
 import com.springBoot.crudDemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -26,5 +29,14 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("From Student order by lastName", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }
